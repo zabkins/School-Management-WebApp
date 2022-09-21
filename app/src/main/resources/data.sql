7b35657e235a2c70709e37a37229e023a084fbda
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-21 20:05:20 +0200

    Fixes roles descriptions in inserts

--- app/src/main/resources/data.sql
+++ app/src/main/resources/data.sql
@@ -1,6 +1,6 @@
-INSERT INTO authority(role) VALUES ('STUDENT');
-INSERT INTO authority(role) VALUES ('TEACHER');
-INSERT INTO authority(role) VALUES ('ADMIN');
+INSERT INTO authority(role) VALUES ('ROLE_STUDENT');
+INSERT INTO authority(role) VALUES ('ROLE_TEACHER');
+INSERT INTO authority(role) VALUES ('ROLE_ADMIN');
 
 INSERT INTO account(username, password, active) VALUES ('teststudent','$2a$10$KuowKbe/p.vz6/Y7XvSo9eJFJXRw.xpoLXinTJ9URfJMzBwzAKdCC',true);
 INSERT INTO account(username, password, active) VALUES ('testteacher','$2a$10$jkN2zgGoqHcqBfEYHxTzj.eBAyB5v05ZWl8UdYPzPXZyBhDrtzBLe',true);
