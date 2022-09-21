c400441d69a27abfaa05b24c764d317b79ce6ecd
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-21 19:31:44 +0200

    Adds UserDetails implementation

---
+++ app/src/main/java/pl/zarczynski/smapp/security/MyUserDetails.java
@@ -0,0 +1,62 @@
+package pl.zarczynski.smapp.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import pl.zarczynski.smapp.domain.security.Account;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MyUserDetails implements UserDetails {
+
+    private String username;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
+
+    public MyUserDetails(Account account){
+        this.username = account.getUsername();
+        this.password = account.getPassword();
+        this.active = account.isActive();
+        this.authorities = account.getAuthorities().stream()
+                .map(auth -> new SimpleGrantedAuthority(auth.getRole()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.active;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
