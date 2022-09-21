8d1005a99597e4067a68e8ca79fe29d57f5f949d
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-21 19:28:03 +0200

    Adds initial WebSecurityConfiguration

---
+++ app/src/main/java/pl/zarczynski/smapp/config/WebSecurityConfig.java
@@ -0,0 +1,36 @@
+package pl.zarczynski.smapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .and()
+                .formLogin()
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().antMatchers("/h2-console/**");
+    }
+}
