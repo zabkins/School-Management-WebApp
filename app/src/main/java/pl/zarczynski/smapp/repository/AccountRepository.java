b7cebe4915c1ce6a31c6304629afd6ba7c74f84d
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-21 20:05:47 +0200

    Adds AccountRepository

--- app/src/main/java/pl/zarczynski/smapp/repository/AccountRepository.java
+++ app/src/main/java/pl/zarczynski/smapp/repository/AccountRepository.java
@@ -1,2 +1,14 @@
-package pl.zarczynski.smapp.repository;public interface AccountRepository {
+package pl.zarczynski.smapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pl.zarczynski.smapp.domain.security.Account;
+
+import java.util.Optional;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    @Query("SELECT a FROM Account a LEFT JOIN FETCH a.authorities WHERE a.username = :username")
+    Optional<Account> findByUsername(String username);
 }
