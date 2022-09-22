8dd554458f539adff35bfe0dd069d9ed79847b50
Michal Zarczynski <mzarczynski29@gmail.com>
2022-09-22 15:14:33 +0200

    Adds Repositories

--- app/src/main/java/pl/zarczynski/smapp/repository/StudentRepository.java
+++ app/src/main/java/pl/zarczynski/smapp/repository/StudentRepository.java
@@ -1,4 +1,10 @@
 package pl.zarczynski.smapp.repository;
 
-public interface StudentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.zarczynski.smapp.domain.entity.Student;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+    Student findByAccountId(Long id);
 }
--- app/src/main/java/pl/zarczynski/smapp/repository/TeacherRepository.java
+++ app/src/main/java/pl/zarczynski/smapp/repository/TeacherRepository.java
@@ -1,2 +1,10 @@
-package pl.zarczynski.smapp.repository;public interface TeacherRepository {
+package pl.zarczynski.smapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.zarczynski.smapp.domain.entity.Teacher;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Teacher findByAccountId(Long id);
 }
